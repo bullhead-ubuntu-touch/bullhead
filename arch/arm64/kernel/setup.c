93f64d716b4b73feef5d238ccbdf44044f964d71
Walid HAMMAMI <walid.ham@hotmail.com>
2022-09-22 09:24:23 +0100

    Undefinied Screen infor

--- arch/arm64/kernel/setup.c
+++ arch/arm64/kernel/setup.c
@@ -591,6 +591,17 @@ const struct seq_operations cpuinfo_op = {
 	.show	= c_show
 };
 
+#if defined(CONFIG_VGA_CONSOLE) || defined(CONFIG_DUMMY_CONSOLE)
+struct screen_info screen_info = {
+	.orig_video_lines = 30,
+	.orig_video_cols = 80,
+	.orig_video_mode = 0,
+	.orig_video_ega_bx = 0,
+	.orig_video_isVGA = 1,
+	.orig_video_points = 8
+};
+#endif 
+
 void arch_setup_pdev_archdata(struct platform_device *pdev)
 {
 	pdev->archdata.dma_mask = DMA_BIT_MASK(32);
