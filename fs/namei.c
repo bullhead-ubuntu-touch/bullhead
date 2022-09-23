ae39dd6d5aa28ee456cc36c9d47ce63d94093e64
Walid HAMMAMI <walid.ham@hotmail.com>
2022-09-23 22:51:40 +0100

    Revert "allow build_open_flags() to return an error"
    This reverts commit d1aa0ae18b7cb98839ad717535df005a2dd7a04b.

--- fs/namei.c
+++ fs/namei.c
@@ -3103,10 +3103,9 @@ out:
 }
 
 struct file *do_filp_open(int dfd, struct filename *pathname,
-		const struct open_flags *op)
+		const struct open_flags *op, int flags)
 {
 	struct nameidata nd;
-	int flags = op->lookup_flags;
 	struct file *filp;
 
 	filp = path_openat(dfd, pathname, &nd, op, flags | LOOKUP_RCU);
@@ -3118,15 +3117,16 @@ struct file *do_filp_open(int dfd, struct filename *pathname,
 }
 
 struct file *do_file_open_root(struct dentry *dentry, struct vfsmount *mnt,
-		const char *name, const struct open_flags *op)
+		const char *name, const struct open_flags *op, int flags)
 {
 	struct nameidata nd;
 	struct file *file;
 	struct filename filename = { .name = name };
-	int flags = op->lookup_flags | LOOKUP_ROOT;
 
 	nd.root.mnt = mnt;
 	nd.root.dentry = dentry;
+
+	flags |= LOOKUP_ROOT;
 
 	if (dentry->d_inode->i_op->follow_link && op->intent & LOOKUP_OPEN)
 		return ERR_PTR(-ELOOP);
